48631778b124ffb1e2a62b5ed42c20a0f70aec2b
piotrkaliszuk <108789004+piotrkaliszuk@users.noreply.github.com>
2022-07-06 13:42:21 +0200

    Add files via upload

--- ROI.py
+++ ROI.py
@@ -5,7 +5,7 @@ def wykrywacz_ruchu():
     import os
 
     img_counter = 1
-    path = '/home/piotrek/PycharmProjects/pythonProject2/Zrzuty'
+    path = '/home/piotrek/PycharmProjects/pythonProject2'
 
     def takePhoto():
         format = now.strftime("%m%d%Y%H%M%S")
@@ -13,11 +13,63 @@ def wykrywacz_ruchu():
         dt = now.strftime("%m/%d/%Y %H:%M:%S")
         cv2.putText(frame, "{}".format(dt), (10, 20),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        img = f"SKW{format}.png"
-        print(f'Zapisano zrzut ekranu: {img}')
-        cv2.imwrite(os.path.join(path, img), frame)
+        takePhoto.img = f"SKW{format}.png"
+        print(f'Zapisano zrzut ekranu: {takePhoto.img}')
+        cv2.imwrite(os.path.join(path, takePhoto.img), frame)
         print(f'Zapisano {img_counter} zrzut!')
 
+    def wyslijmail():
+        import email, smtplib, ssl
+        from email import encoders
+        from email.mime.base import MIMEBase
+        from email.mime.multipart import MIMEMultipart
+        from email.mime.text import MIMEText
+
+        subject = "Wykryto ruch w twoim salonie!"
+        body = f"Złapano zdjęcie włamywacza! captured by Fotopułapka at {format} "
+        html = """\
+        <html>  
+          <body>
+          
+            
+            <p>
+               Jeżeli chciałbyś zobaczyć więcej projektów odwiedź strone poniżej!<br>
+               <a href="https://github.com/piotrkaliszuk">CLICK</a> <br>
+               Piotr Kaliszuk <br>
+               Head of IT AMW <br>
+               Telefon: 512 241 743<br>
+               kontakt: kaliszuk_piotr@wp.pl
+               
+            </p>
+            <img src="amw.jpg">
+          </body>
+        </html>
+        """
+        sender_email = "pkaliszak11@gmail.com"
+        receiver_email = "pkaliszak11@gmail.com"
+        password = "mxfovrnvqustqzau"
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = subject
+        message["Bcc"] = receiver_email
+        message.attach(MIMEText(body, "plain"))
+        message.attach(MIMEText(html,"html"))
+        filename = takePhoto.img
+        with open(filename, "rb") as attachment:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {filename}",
+        )
+        message.attach(part)
+        text = message.as_string()
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, text)
     def frameDiff(It0, It1, It2):
         dI1 = cv2.absdiff(It2, It1)
         dI2 = cv2.absdiff(It1, It0)
@@ -73,6 +125,7 @@ def wykrywacz_ruchu():
                 motionCounter += 1
                 if motionCounter >= 5:
                     takePhoto()
+                    wyslijmail()
                     lastUploaded = now
                     motionCounter = 0
                     img_counter += 1
@@ -85,3 +138,4 @@ def wykrywacz_ruchu():
             break
     cam.release()
     cv2.destroyAllWindows()
+wykrywacz_ruchu()
